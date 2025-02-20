887acbaf3cd750ac4f61f73ce0bf6edd8731ebc4
Jason C. <Jason.C@panza.ai>
2025-02-20 13:11:52 -0500

    Updated URL call to proxy server

--- frontend/config.js
+++ frontend/config.js
@@ -1,3 +1,3 @@
 const config = {
-    API_URL: "https://azurefunchtmlresume.azurewebsites.net/api/visitor-counter"
+    API_URL: "https://expressproxy-h2hwbngsh7h4b8at.eastus2-01.azurewebsites.net/api/callFunction"
 };
